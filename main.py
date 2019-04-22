85693fcd840f8a85b85bbcd9565d8fceaad15109
Eby Sofyan Fadly <eby.sofyan@gmail.com>
2019-04-23 01:03:02 +0800

    fix : fix main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from parser import Parser
 
 from crawler import Crawler
+from sender import Sender
 
 if __name__ == "__main__":
+    import os
     import sys
     import time
 
     TARGET_URL = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp.json'
+    API_URL = 'http://localhost:8000/api/pemilu2019/save'
+    AUTH_TOKEN = os.environ['AUTH_TOKEN']
 
     try:
         TIME_SLEEP = int(sys.argv[1])
@@ -14,12 +18,15 @@ if __name__ == "__main__":
         raise ValueError('TIME SLEEP must an Integer')
 
     while True:
-        keeper = Crawler(TARGET_URL, verify_ssl=False)
-        data = keeper.get_json_data()
+        crawler = Crawler(TARGET_URL, verify_ssl=False)
+        data = crawler.get_json_data()
 
         parser = Parser(data)
-        result = parser.parse()
+        res_data = parser.parse()
 
-        print('Sent!')
+        sender = Sender(API_URL, AUTH_TOKEN)
+        req = sender.send(res_data)
+
+        print(f'Code {req.status_code}')
 
         time.sleep(TIME_SLEEP)
--- sender.py
+++ sender.py
@@ -1,14 +1,16 @@
 import requests
-import os
+import json
 
 
 class Sender(object):
-    def __init__(self, post_url: str):
+    def __init__(self, post_url: str, auth_token: str):
         self._post_url = post_url
+        self._auth_token = auth_token
 
     def send(self, data: dict):
-        header = {
-            ''
+        headers = {
+            'Authorization': f'Token {self._auth_token}',
+            'Content-Type': 'application/json',
         }
-        req = requests.post(self._post_url)
-        
+        req = requests.post(self._post_url, data=json.dumps(data), headers=headers)
+        return req
